77af733f033c1a196ffc803a28ab6288d3f92865
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-06-19 16:41:32 -0400

    Utilizando padrão DTO para retornar Usuários

--- src/main/java/com/marlon/coursemongo/resources/UserResource.java
+++ src/main/java/com/marlon/coursemongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.marlon.coursemongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marlon.coursemongo.domain.User;
+import com.marlon.coursemongo.dto.UserDTO;
 import com.marlon.coursemongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
