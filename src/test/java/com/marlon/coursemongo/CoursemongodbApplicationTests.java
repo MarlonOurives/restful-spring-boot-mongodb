945df5b5b92469c30eaa2bfc392be5977de9b77c
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-06-19 15:53:33 -0400

    Projeto criado

---
+++ src/test/java/com/marlon/coursemongo/CoursemongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlon.coursemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
